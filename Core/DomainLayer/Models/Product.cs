2fd771f915dfdd912aa7adff0a402073a9bdb4d2
Mohamed Gouda <themohamedgouda@gmail.com>
2025-04-16 18:05:39 +0200

    Part 04 Product Module

---
+++ Core/DomainLayer/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models
+{
+    public class Product : BaseEntity<int>
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string PictureUrl { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public ProductBrand ProductBrand { get; set; }
+        public int BrandId  { get; set; } //Fk
+        public ProductType ProductType { get; set; }
+        public int TypeId { get; set; } //Fk
+
+
+    }
+}
