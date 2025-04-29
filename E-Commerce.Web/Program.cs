3d44bb5765ec3aa4637132cf5806ff7fbc56c044
Mohamed Gouda <themohamedgouda@gmail.com>
2025-04-29 15:29:24 +0300

    Part 01 Revision

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -39,7 +39,7 @@ namespace E_Commerce.Web
 
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
+                app.UseSwagger(); 
                 app.UseSwaggerUI();
             }
 
