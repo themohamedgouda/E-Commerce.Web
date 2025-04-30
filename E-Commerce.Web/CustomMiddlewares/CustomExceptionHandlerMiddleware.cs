c4dc6a6e9055d3310be1d15eb237a68b7eed787c
Mohamed Gouda <themohamedgouda@gmail.com>
2025-05-01 00:03:59 +0300

    Part 05 Not Found EndPoint

--- E-Commerce.Web/CustomMiddlewares/CustomExceptionHandlerMiddleware.cs
+++ E-Commerce.Web/CustomMiddlewares/CustomExceptionHandlerMiddleware.cs
@@ -16,28 +16,47 @@ namespace E_Commerce.Web.CustomMiddlewares
             try
             {
                 await next.Invoke(httpcontext);
+                await HandleNotFoundAsync(httpcontext);
             }
             catch (Exception ex)
             {
                 logger.LogError(ex, "An error occurred while processing the request.");
-                httpcontext.Response.StatusCode = ex switch {
-                    NotFoundExcpetion => (int)HttpStatusCode.NotFound,
-                    _ => (int)HttpStatusCode.InternalServerError
-                };
+                await HandleExceptionAsync(httpcontext, ex);
+            }
 
+        }
+        private static async Task HandleExceptionAsync(HttpContext httpcontext, Exception ex)
+        {
+            httpcontext.Response.StatusCode = ex switch
+            {
+                NotFoundExcpetion => (int)HttpStatusCode.NotFound,
+                _ => (int)HttpStatusCode.InternalServerError
+            };
+
+            httpcontext.Response.ContentType = "application/json";
+
+            var errorResponse = new ErrorToReturn()
+            {
+                StatusCode = httpcontext.Response.StatusCode,
+                ErrorMessage = ex.Message
+            };
+            //var ResponseToReturn = JsonSerializer.Serialize(errorResponse);
+            //await httpcontext.Response.WriteAsync(ResponseToReturn);
+            await httpcontext.Response.WriteAsJsonAsync(errorResponse);
+        }
+
+        private static async Task HandleNotFoundAsync(HttpContext httpcontext)
+        {
+            if (httpcontext.Response.StatusCode == (int)HttpStatusCode.NotFound)
+            {
                 httpcontext.Response.ContentType = "application/json";
-
                 var errorResponse = new ErrorToReturn()
                 {
                     StatusCode = httpcontext.Response.StatusCode,
-                    ErrorMessage = ex.Message
+                    ErrorMessage = $"End Point {httpcontext.Request.Path} Is not Found"
                 };
-                //var ResponseToReturn = JsonSerializer.Serialize(errorResponse);
-                //await httpcontext.Response.WriteAsync(ResponseToReturn);
                 await httpcontext.Response.WriteAsJsonAsync(errorResponse);
             }
-           
         }
-
     }
 }
