9defb1d49c6bac2e38508bd93a1f976cd355e328
Mohamed Gouda <themohamedgouda@gmail.com>
2025-05-01 00:23:36 +0300

    Part 06 Validation Error Response

--- E-Commerce.Web/Program.cs
+++ E-Commerce.Web/Program.cs
@@ -1,6 +1,8 @@
 using AutoMapper;
 using DomainLayer.Contracts;
 using E_Commerce.Web.CustomMiddlewares;
+using E_Commerce.Web.Factories;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PrecedencesLayer;
 using PrecedencesLayer.Data;
@@ -8,6 +10,8 @@ using PrecedencesLayer.Repositories;
 using ServicesAbstractionLayer;
 using ServicesImplementationLayer;
 using ServicesImplementationLayer.MappingProfiles;
+using Shared.ErrorMoldels;
+using System.Net;
 
 namespace E_Commerce.Web
 {
@@ -34,7 +38,29 @@ namespace E_Commerce.Web
             builder.Services.AddScoped<IDataSeeding, DataSeeding>();
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();  
             builder.Services.AddAutoMapper(typeof(ServicesImplementationLayer.AssemblyReference).Assembly);
-            builder.Services.AddScoped<IServicesManager, ServicesManager>();  
+            builder.Services.AddScoped<IServicesManager, ServicesManager>();
+            builder.Services.Configure<ApiBehaviorOptions>((options) =>
+            {
+                #region Refactor
+                //{
+                //    var errors = context.ModelState
+                //        .Where(e => e.Value.Errors.Count > 0)
+                //        .Select(e => new ValidationError
+                //        {
+                //            Field = e.Key,                        
+                //            Errors = e.Value.Errors.Select(x => x.ErrorMessage)
+                //        }).ToList();
+                //    var validationToReturn = new ValidationToReturn
+                //    {
+                //        //StatusCode = (int)HttpStatusCode.BadRequest,
+                //        //ErrorMessage = "Validation Errors",
+                //        ValidationErrors = errors
+                //    };
+                //    return new BadRequestObjectResult(validationToReturn);
+                //}; 
+                #endregion
+                options.InvalidModelStateResponseFactory = ApiResponseFactory.GenerateApiValidationErrorResponse;
+            });
             #endregion
 
             var app = builder.Build();
