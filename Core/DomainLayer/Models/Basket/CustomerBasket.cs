145879fbd4941440afd2a842ab3876e147f3e5d2
Mohamed Gouda <themohamedgouda@gmail.com>
2025-05-01 16:54:51 +0300

    Solving Bugs

--- Core/DomainLayer/Models/Basket/CustomerBasket.cs
+++ Core/DomainLayer/Models/Basket/CustomerBasket.cs
@@ -9,6 +9,6 @@ namespace DomainLayer.Models.Basket
     public class CustomerBasket
     {
         public string Id { get; set; }
-        public ICollection<CustomerBasket> Items { get; set; } = [];
+        public ICollection<BasketItem> Items { get; set; } = [];
     }
 }
