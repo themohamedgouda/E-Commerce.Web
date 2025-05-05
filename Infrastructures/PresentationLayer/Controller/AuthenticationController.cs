d79943bc1dd03d66fafc57ee6a7f6491e87b844d
Mohamed Gouda <themohamedgouda@gmail.com>
2025-05-05 17:29:19 +0300

    Part 10 Authentication Controller [Address , Email And Get User ]

--- Infrastructures/PresentationLayer/Controller/AuthenticationController.cs
+++ Infrastructures/PresentationLayer/Controller/AuthenticationController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ServicesAbstractionLayer;
 using Shared.DataTranseferObject.IdentityModuleDto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,6 +26,39 @@ namespace PresentationLayer.Controller
             var result = await servicesManager.AuthenticationService.RegisterAsync(registerDto);
             return Ok(result);
         }
+        [HttpGet("CheckEmail")]
+        public async Task<ActionResult<bool>> CheckEmail(string email)
+        {
+            var result = await servicesManager.AuthenticationService.CheckEmailAsync(email);
+            return Ok(result);
+        }
+        [Authorize]
+        [HttpGet("CurrentUser")]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var result = await servicesManager.AuthenticationService.GetCurrentUserAsync(email!);
+            return Ok(result);
+        }
+        [Authorize]
+        [HttpGet("Address")]
+        public async Task<ActionResult<AdderessDto>> GetCurrentUserAddress()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var result = await servicesManager.AuthenticationService.GetCurrentUserAdderessAsync(email!);
+            return Ok(result);
+        }
+        [Authorize]
+        [HttpGet("Orders")]
+        public async Task<ActionResult<AdderessDto>> UpdateCurrentUserAddress(AdderessDto adderessDto)
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);
+            var result = await servicesManager.AuthenticationService.UpdatedCurrentUserAdderessAsync(email!, adderessDto);
+            return Ok(result);
+
+        }
+
+
 
 
     }
