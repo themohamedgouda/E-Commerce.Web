78dfe34f055e8e658fb6d041e73c2c668b4e0cd7
Mohamed Gouda <themohamedgouda@gmail.com>
2025-05-05 09:53:11 +0300

    Part 06 Authentication Controller [Login & Register]

---
+++ Infrastructures/PresentationLayer/Controller/AuthenticationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServicesAbstractionLayer;
+using Shared.DataTranseferObject.IdentityModuleDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Controller
+{
+ 
+    public class AuthenticationController(IServicesManager servicesManager) : APIBaseController
+    {
+        [HttpPost("Login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var result = await servicesManager.AuthenticationService.LoginAsync(loginDto);
+            return Ok(result);
+        }
+        [HttpPost("Register")]
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            var result = await servicesManager.AuthenticationService.RegisterAsync(registerDto);
+            return Ok(result);
+        }
+
+
+    }
+}
