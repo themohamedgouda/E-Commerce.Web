0166d76b2141be432943f5354042649897cb7f08
Mohamed Gouda <themohamedgouda@gmail.com>
2025-05-06 10:35:48 +0300

    Part 02 Order Module Entities

---
+++ Core/DomainLayer/Models/Order/Order.cs
@@ -0,0 +1,26 @@
+﻿using DomainLayer.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.Order
+{
+    public class Order : BaseEntity<Guid>
+    {
+        public string UserEmail { get; set; } = string.Empty;
+        public DateTimeOffset OrderDate { get; set; }
+        public OrderAddress Address { get; set; } = default!;
+        public DelivaryMethod DelivaryMethod { get; set; } = default!;
+        public int DelivaryMethodId { get; set; } // FK
+        public OrderStatus OrderStatus { get; set; }
+        public ICollection<OrderItem> Items { get; set; } = [];
+        public decimal SubTotal { get; set; }
+        //[NotMapped]
+        //public decimal Total { get => SubTotal + DelivaryMethod.Price;}
+        public decimal GetTotal() =>  SubTotal + DelivaryMethod.Price;
+       
+    }
+}
---
+++ Core/DomainLayer/Models/Order/ProductItemOrdered.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Models.Order
+{
+    public class ProductItemOrdered : BaseEntity<int>
+    {
+      
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string PictureUrl { get; set; } = string.Empty;
+    }
+}
