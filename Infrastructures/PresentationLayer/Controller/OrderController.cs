9142910dd6731b719b9a01443b4de67de9b99134
Mohamed Gouda <themohamedgouda@gmail.com>
2025-05-06 13:55:48 +0300

    Part 06 Order Controller [ Create Order ]

---
+++ Infrastructures/PresentationLayer/Controller/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ServicesAbstractionLayer;
+using Shared.DataTranseferObject.OrderModuleDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PresentationLayer.Controller
+{
+    public class OrderController(IServicesManager _servicesManager) : APIBaseController
+    {
+        [Authorize]
+        [HttpPost]
+        public async Task<ActionResult<OrderToReturnDto>> CreateOrder([FromBody] OrderDto orderDto)
+        {
+            var Email = GetUserEmailFromToken();
+            var order = await _servicesManager.OrderServices.CreateOrderAsync(orderDto, Email);
+            return Ok(order);
+
+
+        }
+
+    }
+}
